9b55aeccc6614d2b611d1969b11437dbc9ee754b
Farida-Hamid <faridaaddala@gmail.com>
2022-10-25 17:49:46 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -14,3 +14,23 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 UPDATE animals
   SET name = 'Gabumon' WHERE name = 'GABUMON';
    
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+  VALUES ('Charmander', '2020-02-08', 11.0, FALSE, 0);
+  
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+  VALUES ('Plantmon', '2021-01-15', 5.7, True, 2);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+  VALUES ('Squirtle', '1993-04-02', 12.13, FALSE, 3);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+  VALUES ('Angemon', '2005-05-12', 5.7, True, 1);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+  VALUES ('Boarmon', '2005-05-07', 20.4, True, 7);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+  VALUES ('Blossom', '1998-10-13', 17.0, True, 3);
+  
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+  VALUES ('Ditto', '2022-05-14', 22.0, True, 4);
