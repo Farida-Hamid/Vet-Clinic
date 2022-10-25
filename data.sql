4f088b3a93d06f9aff6cce13078f99285d39a691
Farida-Hamid <faridaaddala@gmail.com>
2022-10-25 14:29:09 +0300

    Insert data

---
+++ data.sql
@@ -0,0 +1,11 @@
+vet_clinic=# INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+              VALUES ('Agumon', '2020-02-03', 10.23, TRUE, 0);
+
+vet_clinic=# INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+              VALUES ('GABUMON', '2018-11-15', 8.0, TRUE, 2);
+
+vet_clinic=# INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+              VALUES ('Pikachu', '2021-01-07', 15.04, FALSE, 1);
+
+vet_clinic=# INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+               VALUES ('Devimon', '2017-05-12', 11.0, TRUE, 5);
