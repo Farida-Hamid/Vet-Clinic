95d7f68c72aa1ab3b088b266f7f76f875fe285c3
Farida-Hamid <faridaaddala@gmail.com>
2022-10-25 14:16:40 +0300

    Create table with id as identity

--- schema.sql
+++ schema.sql
@@ -2,9 +2,9 @@ postgres=# CREATE DATABASE vet_clinic;
 postgres=# \c vet_clinic
 
 vet_clinic=# CREATE TABLE animals (
-vet_clinic(# id INT,
-vet_clinic(# name VARCHAR(25),
-vet_clinic(# date_of_birth TIMESTAMP,
-vet_clinic(# escape_attempts INT,
-vet_clinic(# neutered BOOLEAN,
-vet_clinic(# weight_kg DECIMAL);
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(25),
+  date_of_birth TIMESTAMP,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL);
